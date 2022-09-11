cf46d9d53a839eeb037e59eaa84e589db4dec027
AnthonyQY <contact@lolitek.com>
2022-09-10 17:11:19 -0700

    Applied fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap"
           rel="stylesheet"
         />
       </Head>
